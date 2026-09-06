17cd66d7f8a5647636c347b12ee1cd23311fd33b
Alexandr Slelin <a.slelin.work@mail.ru>
2026-09-06 17:44:07 +0300

    add: Tag.java

---
+++ src/main/java/a/slelin/work/word/master/entity/Tag.java
@@ -0,0 +1,32 @@
+package a.slelin.work.word.master.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = Tag.TABLE_NAME)
+@Entity(name = Tag.ENTITY_NAME)
+public class Tag {
+
+    public static final String ENTITY_NAME = "Tag";
+
+    public static final String TABLE_NAME = "tag";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    @Column(nullable = false,
+            unique = true)
+    private String name;
+}
