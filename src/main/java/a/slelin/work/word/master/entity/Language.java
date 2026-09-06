6991e7ab1cbe794027dc400537adc2c8c11ba788
Alexandr Slelin <a.slelin.work@mail.ru>
2026-09-06 17:31:05 +0300

    add: Language.java

---
+++ src/main/java/a/slelin/work/word/master/entity/Language.java
@@ -0,0 +1,36 @@
+package a.slelin.work.word.master.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = Language.TABLE_NAME)
+@Entity(name = Language.ENTITY_NAME)
+public class Language {
+
+    public static final String ENTITY_NAME = "Language";
+
+    public static final String TABLE_NAME = "language";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    @Column(nullable = false)
+    private String name;
+}
