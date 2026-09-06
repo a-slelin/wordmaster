f8072118ff06a6e18a420cdfb28eeab50e5d7dfb
Alexandr Slelin <a.slelin.work@mail.ru>
2026-09-06 18:10:06 +0300

    add: Card.java

---
+++ src/main/java/a/slelin/work/word/master/entity/Card.java
@@ -0,0 +1,62 @@
+package a.slelin.work.word.master.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Entity(name = Card.ENTITY_NAME)
+@EqualsAndHashCode(callSuper = false)
+@Table(name = Card.TABLE_NAME)
+public class Card extends Audit {
+
+    public static final String ENTITY_NAME = "Card";
+
+    public static final String TABLE_NAME = "card";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "deck_id", nullable = false)
+    private Deck deck;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    @Column(nullable = false)
+    private String word;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    @Column(nullable = false)
+    private String translation;
+
+    private String transcription;
+
+    private String example_sentence;
+
+    @URL
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @URL
+    @Column(name = "audio_url")
+    private String audioUrl;
+
+    @Min(1)
+    @Column(nullable = false)
+    private Long position;
+}
